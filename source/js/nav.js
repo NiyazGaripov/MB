75267a9694052594c6acc1f251dd20d37f548c21
Niyaz Garipov <Niayz_Indicus@mail.ru>
2019-12-02 22:26:29 +0300

    Adds script for toggle mobile menu

---
+++ source/js/nav.js
@@ -0,0 +1,17 @@
+var header = document.querySelector(".header");
+var toggle = document.querySelector(".header__toggle");
+
+header.classList.remove("header--nojs");
+
+toggle.addEventListener('click', function (evt) {
+  evt.preventDefault();
+
+  if (header.classList.contains("header--closed")) {
+    header.classList.remove("header--closed");
+    header.classList.add("header--opened");
+  } else {
+    header.classList.add("header--closed");
+    header.classList.remove("header--opened");
+  }
+});
+
